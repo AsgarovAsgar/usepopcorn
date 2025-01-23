eebc94f54ae25c9d46b156f314525f4f7e9ae8af
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-01-23 12:31:18 +0400

    add experimental index.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
-import TextExpander from "./TextExpander";
-
-function Test() {
-  const [movieRating, setMovieRating] = React.useState(0);
-
-  return (
-    <div>
-      <StarRating
-        maxRating={10}
-        className="test"
-        onSetRating={setMovieRating}
-      />
-      <p>This movie rated {movieRating} stars</p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <StarRating
-      maxRating={5}
-      color="red"
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    />
-    <Test /> */}
-
-    <TextExpander>
-      Space travel is the ultimate adventure! Imagine soaring past the stars and
-      exploring new worlds. It's the stuff of dreams and science fiction, but
-      believe it or not, space travel is a real thing. Humans and robots are
-      constantly venturing out into the cosmos to uncover its secrets and push
-      the boundaries of what's possible.
-    </TextExpander>
+    <App />
   </React.StrictMode>
 );
